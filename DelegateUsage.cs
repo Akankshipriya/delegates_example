a7550e642c04f4427e614d58730f9d9b4dd93ade
Akankshi Priya <akankshipriya24@gmail.com>
2022-09-07 11:18:59 +0530

    Add files via upload

---
+++ DelegateUsage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate bool IsPromotable(Employee emp1);
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<Employee> employeeList, IsPromotable IsEligibleToPromote)
+        {
+            foreach(Employee employee in employeeList)
+            {
+                if (IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void HelloFunctionDelegate(string Message);
+    class Program
+    { 
+        public static bool Promote(Employee emp)
+        {
+            if (emp.Experience>=5)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+        static void Main(string[] args)
+        {
+            //Delegate Introduction
+            Program p = new Program();
+            HelloFunctionDelegate del = new HelloFunctionDelegate(p.Hello);
+            del("Akankshi");
+
+            //Deletgate Usage
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5 });
+            employees.Add(new Employee() { ID = 102, Name = "Maryam", Salary = 6000, Experience = 7 });
+            employees.Add(new Employee() { ID = 103, Name = "Adam", Salary = 7000, Experience = 4 });
+
+            IsPromotable isPromotable = new IsPromotable(Promote);
+            Employee.PromoteEmployee(employees,isPromotable);
+            Console.ReadLine();
+        }
+
+    }
+}
